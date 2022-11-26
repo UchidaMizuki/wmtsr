27c657a9a74823beab2a11dd70a03b17b1514480
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-26 15:10:45 +0900

    Update logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -4,15 +4,13 @@ library(magick)
 library(sysfonts)
 library(fs)
 
-# logo --------------------------------------------------------------------
-
-path_image <- "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Wombat_(PSF).png/313px-Wombat_(PSF).png"
-image <- image_read(path_image)
-
-# font <- font_files()
 font_add("Segoe UI", "segoeui.ttf")
 
-sticker(image,
+# logo --------------------------------------------------------------------
+
+image_wombat <- image_read("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Wombat_(PSF).png/313px-Wombat_(PSF).png")
+
+sticker(image_wombat,
         package = "wombats",
         filename = path("man/figures/logo.png"),
         s_width = 1.25,
