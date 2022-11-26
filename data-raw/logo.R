f3c4bb6c7804804767a5efded696c654e4820c58
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-26 15:41:36 +0900

    Update logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -4,7 +4,7 @@ library(magick)
 library(sysfonts)
 library(fs)
 
-font_add("Segoe UI", "segoeui.ttf")
+font_add("Century Gothic", "GOTHIC.TTF")
 
 # logo --------------------------------------------------------------------
 
@@ -13,12 +13,13 @@ image_wombat <- image_read("https://upload.wikimedia.org/wikipedia/commons/thumb
 sticker(image_wombat,
         package = "wombats",
         filename = path("man/figures/logo.png"),
-        s_width = 1.25,
-        s_height = 1.25,
+        s_width = 1.3,
+        s_height = 1.3,
         s_x = 1,
-        s_y = 0.75,
+        s_y = 1.15,
         p_size = 25,
         h_fill = "snow",
         h_color = "dimgray",
         p_color = "dimgray",
-        p_family = "Segoe UI")
+        p_y = 0.6,
+        p_family = "Century Gothic")
