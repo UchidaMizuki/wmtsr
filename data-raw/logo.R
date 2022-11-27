4952a0bb6ee65c3758a39a04fb171fef7edc5420
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-27 23:40:53 +0900

    Update logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -1,25 +1,32 @@
 library(tidyverse)
 library(hexSticker)
 library(magick)
-library(sysfonts)
 library(fs)
 
-font_add("Century Gothic", "GOTHIC.TTF")
+font_logo <- "Poppins"
+sysfonts::font_add_google(font_logo)
 
 # logo --------------------------------------------------------------------
 
+file_logo <- "man/figures/logo.png"
 image_wombat <- image_read("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Wombat_(PSF).png/313px-Wombat_(PSF).png")
 
 sticker(image_wombat,
         package = "wombats",
-        filename = path("man/figures/logo.png"),
+        filename = file_logo,
+
         s_width = 1.3,
         s_height = 1.3,
         s_x = 1,
         s_y = 1.15,
-        p_size = 25,
-        h_fill = "snow",
-        h_color = "dimgray",
+
+        p_size = 23,
         p_color = "dimgray",
         p_y = 0.6,
-        p_family = "Century Gothic")
+        p_family = font_logo,
+        p_fontface = "bold.italic",
+
+        h_fill = "snow",
+        h_color = "dimgray")
+
+usethis::use_logo(file_logo)
